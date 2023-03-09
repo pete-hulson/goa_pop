f962a194018ba34e9249b0c7dd376324db304d25
Ben <ben.williams@noaa.gov>
2023-03-09 10:17:30 -0900

    init code

---
+++ 2023/R/2023_analysis.R
@@ -0,0 +1,16 @@
+# notes ----
+# code for 2023 goa pop data queries and such 
+# pete.hulson@noaa.gov
+# ben.williams@noaa.gov
+
+# load ----
+library(afscdata)
+
+# globals 
+year = 2023
+
+# setup folder structure
+# setup_folders(year)
+
+# query data 
+goa_pop(year, )
