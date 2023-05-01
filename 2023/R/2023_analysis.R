c48f99745cfd7973f14824fc2206694e9050569e
Ben <ben.williams@noaa.gov>
2023-05-01 15:55:50 -0800

    init code

--- 2023/R/2023_analysis.R
+++ 2023/R/2023_analysis.R
@@ -13,4 +13,16 @@ year = 2023
 # setup_folders(year)
 
 # query data 
-goa_pop(year, )
+goa_pop(year, off_yr=FALSE)
+
+
+# globals ----
+species = "POPA"
+area = "GOA"
+afsc_species = 30060
+norpac_species = 301
+
+akfin = connect()
+q_catch(year=year, species=species, area=area, db=akfin)
+q_fish_obs(year=year, species=norpac_species, area=area, db=akfin)
+q_bts_biomass(year=year, area=area, species=afsc_species, by='total', db=akfin) 
