d7f758a397c400983cc3ff57c897bca3a538ab26
Ben <ben.williams@noaa.gov>
2023-05-02 08:03:24 -0800

    examples

--- 2023/R/2023_analysis.R
+++ 2023/R/2023_analysis.R
@@ -5,15 +5,24 @@
 # ben.williams@noaa.gov
 
 # load ----
-devtools::unload("afscdata")
+# devtools::unload("afscassess")
 library(afscdata)
+library(afscassess)
 
-# globals 
+# previous accepted model
+# this is more for an example since the previous assessment was not in the afscdata framework
+# accepted_model(base_year=2021, base_model="model_20_1", year=2023)
+
+# globals ----
 year = 2023
+TAC = c(31238, 36177, 38268) # previous 3 years
+species = "POP"
 
-# setup folder structure
+# setup folder structure - only run this once
 # setup_folders(year)
 
-# query data 
+# query data ----
 goa_pop(year)
 
+# clean data ----
+clean_catch(year=year, species=species, TAC=TAC)
