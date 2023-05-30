05a5d94c9bfe1ab015029c7fe83da6e94b131474
Maia Kapur <39382798+mkapur-noaa@users.noreply.github.com>
2023-05-30 13:58:24 -0700

    add season for catch and cpue data
    need to confirm what is appropriate here, 1 and 7 as placeholders for now

--- 2023/R/ss_dataprep.R
+++ 2023/R/ss_dataprep.R
@@ -4,6 +4,28 @@
 require(dplyr)
 require(here)
 
+## catches ----
+## these begin in 1991
+catches_new <- read.csv(here('2023','data','raw','fsh_catch_data.csv'))
+catches_old <- scan(here('2023','base','goa_pop_2021.dat'), skip = 46, nlines = 1)
+
+catches_91 <- catches_new %>%  
+  group_by(year) %>%
+  summarise(catch = round(sum(weight_posted ))) %>% 
+  select(Yr = year, catch) %>%
+  arrange(Yr)
+
+cbind(catch_old = catches_old, Yr = 1961:2021) %>%
+  merge(., catches_91, by = 'Yr', all = T) %>%
+  mutate(catch == catch_old) ## double check that overlapping years match
+
+cbind(catch_old = catches_old, Yr = 1961:2021) %>%
+  merge(., catches_91, by = 'Yr', all = T) %>%
+  mutate(seas = 1, catch_use = ifelse(Yr <1991, catch_old, catch), fleet = 1, cv = 0.01) %>% 
+  select(Yr, seas, fleet, catch = catch_use, cv) %>%
+  write.csv(.,here('2023','data','for_ss',paste0(Sys.Date(),'-catches.csv')))
+
+
 ## survey obs ----
 ## there is a singleLL survey observation in 1999 in an observed mt of 1000 (?) and SE 100
 trawl <- read.csv(here('2023','data','raw','goa_total_bts_biomass_data.csv'))
@@ -13,7 +35,7 @@ trawl %>%
          cpue = round(total_biomass),
          se = (total_biomass-min_biomass)/1.96,
          cv = round(se/total_biomass,2)) %>%
-  select(Yr = year, fleet, cpue, cv) %>%
+  select(Yr = year, seas = 7, fleet, cpue, cv) %>%
   arrange(Yr) %>%
   filter(Yr >= 1990) %>%
   write.csv(.,here('2023','data','for_ss',paste0(Sys.Date(),'-cpue.csv')))
