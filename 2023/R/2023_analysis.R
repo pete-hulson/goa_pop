bdf54a9e1a0492faf762f4aa510653720cfb7e3d
pete-hulson <pete.hulson@noaa.gov>
2023-06-27 10:58:49 -0800

    finalized data pull script, added ctl fcn, ran prelim 2023 base model

--- 2023/R/2023_analysis.R
+++ 2023/R/2023_analysis.R
@@ -6,8 +6,8 @@
 
 # load ----
 
-devtools::unload("afscdata")
-devtools::unload("afscassess")
+# devtools::unload("afscdata")
+# devtools::unload("afscassess")
 
 devtools::install_github("afsc-assessments/afscdata", force = TRUE)
 devtools::install_github("BenWilliams-NOAA/afscassess@devph", force = TRUE)
@@ -40,70 +40,83 @@ afscassess::sp_switch(species)
 ## you must be on the VPN for this to work
 afscdata::goa_pop(year)
 
-# get data file parts ----
+# get data files together (dat and ctl) ----
 
 # weight-at-age
 # note from ben on these admb called functions: !!! I'm having trouble running this function via R2admb so stepped out and ran it command line, works fine if I compile it command line and then use the R2admb run function, maybe I'll pass the .exe instead of rebuilding the .tpl each year?
-waa <- afscassess::weight_at_age(year=year, admb_home=admb_home, rec_age=rec_age, area = "goa")
+afscassess::weight_at_age(year = year,
+                          admb_home = admb_home,
+                          rec_age = rec_age,
+                          area = "goa")
 
 # fishery catch
-catch <- afscassess::clean_catch(year = year, 
-                                 species = species, 
-                                 TAC = TAC)
+afscassess::clean_catch(year = year, 
+                        species = species, 
+                        TAC = TAC)
 
 # bottom trawl survey biomass
-bts_biom <- afscassess::bts_biomass(year = year, 
-                                    rmv_yrs = c(1984, 1987))
+afscassess::bts_biomass(year = year, 
+                        rmv_yrs = c(1984, 1987))
 
 # fishery age comp
-fish_ac <- afscassess::fish_age_comp(year = year,
-                                     exp_meth = 'marg_len',
-                                     rec_age = rec_age, 
-                                     plus_age = plus_age,
-                                     lenbins = lengths,
-                                     rmv_yrs = c(1987, 1989))
+afscassess::fish_age_comp(year = year,
+                          exp_meth = 'marg_len',
+                          rec_age = rec_age, 
+                          plus_age = plus_age,
+                          lenbins = lengths,
+                          rmv_yrs = c(1987, 1989))
 
 # bottom trawl survey age comp
-bts_ac <- afscassess::bts_age_comp(year = year,
-                                   area = "goa",
-                                   rec_age = rec_age,
-                                   plus_age = plus_age,
-                                   rmv_yrs = c(1984,1987))
+afscassess::bts_age_comp(year = year,
+                         area = "goa",
+                         rec_age = rec_age,
+                         plus_age = plus_age,
+                         rmv_yrs = c(1984,1987))
 
 # fishery size comp
-fish_lc <- afscassess::fish_length_comp_pop(year = year,
-                                            rec_age = rec_age,
-                                            lenbins = lengths,
-                                            rmv_yrs = c(1988, 1993, 1994, 2003, 2007, 2009, 2011, 2013, 2015, 2017, 2019, 2021, 2022, 2023))
+afscassess::fish_length_comp_pop(year = year,
+                                 rec_age = rec_age,
+                                 lenbins = lengths,
+                                 rmv_yrs = c(1988, 1993, 1994, 2003, 2007, 2009, 2011, 2013, 2015, 2017, 2019, 2021, 2022, 2023))
 
 # bottom trawl survey size comp
-bts_lc <- afscassess::bts_length_comp(year = year,
-                                      area = "goa",
-                                      lenbins = lengths)
+afscassess::bts_length_comp(year = year,
+                            area = "goa",
+                            lenbins = lengths)
 
 # 60s size-age matrix
-sz_age_60 <- afscassess::size_at_age_pop_60(year = year,
-                                            rec_age = rec_age,
-                                            lenbins = lengths)
+afscassess::size_at_age_pop_60(year = year,
+                               rec_age = rec_age,
+                               lenbins = lengths)
 
 # current size-age matrix
-sz_age <- afscassess::size_at_age(year = year,
-                                  admb_home = admb_home,
-                                  rec_age = rec_age,
-                                  lenbins = lengths)
+afscassess::size_at_age(year = year,
+                        admb_home = admb_home,
+                        rec_age = rec_age,
+                        lenbins = lengths)
 
 # ageing error matrix
-ae_mtx <- afscassess::age_error(year = year, 
-                                reader_tester = "reader_tester.csv", 
-                                admb_home = admb_home, 
-                                species = species, 
-                                rec_age = rec_age, 
-                                plus_age = plus_age)
+afscassess::age_error(year = year, 
+                      reader_tester = "reader_tester.csv", 
+                      admb_home = admb_home, 
+                      species = species, 
+                      rec_age = rec_age, 
+                      plus_age = plus_age)
 
+# concatenate dat file, for now writing it to output folder in data
+afscassess::concat_dat_pop(year = year,
+                           species = species,
+                           area = "goa",
+                           folder = "data/output",
+                           dat_name = "goa_pop",
+                           rec_age = rec_age,
+                           plus_age = plus_age,
+                           spawn_mo = 5)
 
-
-
-
-
-
+# write ctl file, for now writing it to output folder in data
+afscassess::write_ctl_pop(year = year,
+                          base_mdl_fldr = "2020.1 (2021)",
+                          mdl_name = "Model_1",
+                          ctl_name = "goa_pop",
+                          folder = "data/output")
 
